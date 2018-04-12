12f3dfcba8dc8d7c2aa64819218f20315c3b545d
wangzy <18895613681@163.com>
2018-04-12 16:47:11 +0800

    sqlscript

---
+++ sqlScript/student_info.sql
@@ -0,0 +1,4 @@
+INSERT INTO `student_info` VALUES (20180001, '张三', 0, '计算机', '汉', '团员', '654321');
+INSERT INTO `student_info` VALUES (20180002, '李⑤', 1, '计算机', '汉', '团员', '654321');
+INSERT INTO `student_info` VALUES (20180003, '仙洒', 1, '计算机', '汉', '团员', '654321');
+INSERT INTO `student_info` VALUES (20180004, 'aaa@qq.com', 1, '信息管理', '汉', '团员', '654321');
---
+++ sqlScript/teacher_info.sql
@@ -0,0 +1,3 @@
+INSERT INTO `teacher_info` VALUES (20189001, '大q老师', 0, '汉', '654321');
+INSERT INTO `teacher_info` VALUES (20189002, '大b老师', 1, '汉', '654321');
+INSERT INTO `teacher_info` VALUES (20189003, '大a老师', 0, '汉', '654321');
